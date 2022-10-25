ebea8c1e3f483f0105db8ffd61d6af692257de40
Sourav Saha <shanto.sourav07@gmail.com>
2022-10-25 23:28:47 +0600

    Categories Data Done

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('API Running');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 });
 
 app.listen(port, () => {
