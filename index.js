483dc6bd125c9d8fd53e0766003dbec661a343f2
Sourav Saha <shanto.sourav07@gmail.com>
2022-10-25 21:48:22 +0600

    Express Configuration Done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.listen(port, () => {
+    console.log('API Running on Port', port);
+})
