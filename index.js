b4eecd39adcd307d6410f5423e981bb79ac033b0
Sourav Saha <shanto.sourav07@gmail.com>
2022-10-26 13:03:29 +0600

    Courses Data Done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('API Running');
@@ -14,6 +15,22 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const categoriesID = courses.filter(c => c.category_id === id);
+    res.send(categoriesID);
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const coursesID = courses.find(c => c._id === id);
+    res.send(coursesID);
+})
 
 app.listen(port, () => {
     console.log('API Running on Port', port);
